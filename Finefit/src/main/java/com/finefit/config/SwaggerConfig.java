ab4bf4c7f1351df596d54dac92ee667daabbfa49
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-11-08 03:57:56 +0900

    feat : API 문서화를 위해 SwaggerConfig 클래스 구현

---
+++ Finefit/src/main/java/com/finefit/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.finefit.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .addServersItem(new Server().url("https://api.finefit.co.kr/").description("운영 환경"))
+        .addServersItem(new Server().url("http://localhost:8080/").description("로컬 환경"))
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+        .components(new Components()
+            .addSecuritySchemes("bearerAuth",
+                new SecurityScheme()
+                    .name("access")
+                    .type(Type.APIKEY)
+                    .in(SecurityScheme.In.HEADER)
+                    .bearerFormat("JWT")))
+        .info(new Info()
+            .title("Dear Birdy API")
+            .version("API v1.0")
+            .description("API documentation"));
+  }
+}
