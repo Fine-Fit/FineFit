217b061ae92d9761fe26186381f4e8a22069ac99
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-11-08 04:55:11 +0900

    feat : 요청 성공 실패에 따른 Enum 클래스 상수 추가

--- Finefit/src/main/java/com/finefit/domain/model/type/FailedResultType.java
+++ Finefit/src/main/java/com/finefit/domain/model/type/FailedResultType.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum FailedResultType {
   FAILED_USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않은 사용자 입니다."),
+  FAILED_PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+  FAILED_ACCESS_PENDING(HttpStatus.BAD_REQUEST, "승인 대기중 입니다."),
+  FAILED_ACCESS_REJECTED(HttpStatus.BAD_REQUEST, "승인 거절 되었습니다."),
   ;
 
   private final HttpStatus status;
--- Finefit/src/main/java/com/finefit/domain/model/type/SuccessResultType.java
+++ Finefit/src/main/java/com/finefit/domain/model/type/SuccessResultType.java
@@ -7,6 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum SuccessResultType {
+  SUCCESS_ADMIN_REGISTER(HttpStatus.OK, "관리자 회원가입 요청 성공"),
+  SUCCESS_VALIDATE_ID(HttpStatus.OK, "아이디 중복 체크 성공"),
+  SUCCESS_ADMIN_LOGIN(HttpStatus.OK, "관리자 로그인 성공"),
+  SUCCESS_GET_USER_INFO(HttpStatus.OK, "사용자 정보 조회 성공"),
   ;
 
   private final HttpStatus status;
