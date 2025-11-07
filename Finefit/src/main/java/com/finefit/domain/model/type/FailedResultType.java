7c040b9b24b04c74924069c4e44547e7f7581cd1
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-11-08 05:21:04 +0900

    feat : 요청에 대한 성공 실패에 따른 응답 상수 추가

--- Finefit/src/main/java/com/finefit/domain/model/type/FailedResultType.java
+++ Finefit/src/main/java/com/finefit/domain/model/type/FailedResultType.java
@@ -11,6 +11,9 @@ public enum FailedResultType {
   FAILED_PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
   FAILED_ACCESS_PENDING(HttpStatus.BAD_REQUEST, "승인 대기중 입니다."),
   FAILED_ACCESS_REJECTED(HttpStatus.BAD_REQUEST, "승인 거절 되었습니다."),
+  FAILED_COUNSEL_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 설문지를 찾을 수 없습니다."),
+
+  FAILED_ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
   ;
 
   private final HttpStatus status;
