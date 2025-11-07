8347d8713b9aa94c54aa590eceee594f0189c8be
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-11-08 03:31:11 +0900

    first commit

---
+++ Finefit/src/main/java/com/finefit/FinefitApplication.java
@@ -0,0 +1,13 @@
+package com.finefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinefitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinefitApplication.class, args);
+  }
+
+}
---
+++ Finefit/src/test/java/com/finefit/FinefitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinefitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
